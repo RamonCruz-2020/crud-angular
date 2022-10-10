43b0bef574292216b7044e7e291b66faabe2c336
Ramon Cruz <ramon.rodrigo2020@hotmail.com>
2022-10-10 10:23:47 -0300

    Salvando os dados com HTTP POST

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -10,7 +13,9 @@ export class CourseFormComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar) {
 
     this.form = this.formBuilder.group({
       name: [null],
@@ -24,9 +29,18 @@ export class CourseFormComponent implements OnInit {
 
   onSubmit(){
 
+    this.service.save(this.form.value)
+      .subscribe(result => console.log(result), error => this.onError());
+
   }
 
   onCancel(){
+
+  }
+
+  onError(){
+
+    this.snackBar.open('Erro ao salvar curso','', {duration: 5000});
 
   }
 
